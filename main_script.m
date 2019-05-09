7d38ce92bd26686019fee37bd8005e9ff76779fc
AngeloCa <angelo.canesso@orange.fr>
2019-05-09 16:34:38 +0200

    first

---
+++ main_script.m
@@ -0,0 +1,18 @@
+%% Generating keys
+tic 
+[priv_key, pub_key] = genrsa_ecp_nistp256();
+t1 = toc;
+fprintf('Time required to generate keys is : %fs', t1)
+%% Signing file with previous keys
+file = importdata(file_to_sign.txt);
+tic 
+[signature] = digital_signature(priv_key, file);
+t2 = toc;
+fprintf('Time required for signature is : %fs', t2)
+%% Verify signature
+tic 
+verif_state = verif_signature(pub_key, file);
+t3 = toc;
+fprintf('Times required to verify signature : %fs\n\n', t3)
+%% Ploting glabal parameters
+fprintf("Total elapsed time is %fs", t1+t2+t3)
