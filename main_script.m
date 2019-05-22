1a55bdff7f29def7d88962a6f2879773e731f06d
leandro <leandrones>
2019-05-22 23:19:41 +0200

    test files for signature verification

--- main_script.m
+++ main_script.m
@@ -4,14 +4,14 @@ tic
 t1 = toc;
 fprintf('Time required to generate keys is : %fs', t1)
 %% Signing file with previous keys
-file = importdata(file_to_sign.txt);
+file = "file_to_sign.txt"
 tic 
-[signature] = digital_signature(priv_key_file, file);
+signed_file = digital_signature(priv_key_file, file);
 t2 = toc;
 fprintf('Time required for signature is : %fs', t2)
 %% Verify signature
 tic 
-verif_state = verify_signature(pub_key_file, file);
+verif_state = verify_signature(pub_key_file, file,signed_file);
 t3 = toc;
 fprintf('Times required to verify signature : %fs\n\n', t3)
 %% Ploting glabal parameters
