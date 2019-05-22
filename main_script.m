1e7e7f849bdf627a4b6f4274469f3f488ff81d9c
AngeloCa <angelo.canesso@orange.fr>
2019-05-22 18:17:52 +0200

    upd

--- main_script.m
+++ main_script.m
@@ -2,13 +2,13 @@
 tic 
 [pub_key_file, priv_key_file] = genecp_nistp256();
 t1 = toc;
-fprintf('Time required to generate keys is : %fs', t1)
+fprintf('Time required to generate keys is : %fs\n', t1)
 %% Signing file with previous keys
 file = importdata(file_to_sign.txt);
 tic 
 [signature] = digital_signature(priv_key_file, file);
 t2 = toc;
-fprintf('Time required for signature is : %fs', t2)
+fprintf('Time required for signature is : %fs\n', t2)
 %% Verify signature
 tic 
 verif_state = verify_signature(pub_key_file, file);
