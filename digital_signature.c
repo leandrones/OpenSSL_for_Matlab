83ea50e1438a6026e50fa5fd8e788891d69af695
matman26 <a.matheus26@hotmail.com>
2019-05-23 08:16:08 +0200

    Updating Mesurer

--- digital_signature.c
+++ digital_signature.c
@@ -2,11 +2,12 @@
 #include <openssl/err.h>
 #include <openssl/ec.h>
 #include <openssl/pem.h>
+#include <matrix.h>
 #include <string.h>
 #include "mex.h"
 
-#define ECCTYPE  "prime256v1"
-#define filename "file_to_sign.txt"
+#define ECCTYPE           "prime256v1"
+#define signature_output  "tosign.txt.sha256"
 
 /* Still working on the signing part, just made sure the files are
 read correctly and all variables are there */
@@ -15,14 +16,17 @@ read correctly and all variables are there */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, 
   const mxArray *prhs[]){
 
-  BIO                  *inbio = NULL;
   EVP_PKEY             *pkey   = NULL;
   EC_KEY               *myecc  = NULL;
   EVP_MD_CTX           *mdctx = NULL;
   unsigned char        *sig = NULL;
   long unsigned int    slen;
-  int                  ret = 0;
   int                  eccgrp;
+
+  char *keyFileName;
+  keyFileName = mxArrayToString(prhs[0]);
+  char *filename;
+  filename = mxArrayToString(prhs[1]);
 
   /* ---------------------------------------------------------- *
    * These function calls initialize openssl for correct work.  *
@@ -32,66 +36,14 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
   ERR_load_crypto_strings();
 
   /* ---------------------------------------------------------- *
-   * Create the Input/Output BIO's.                             *
-   * ---------------------------------------------------------- */
-  inbio  = BIO_new(BIO_s_file());
-  inbio = BIO_new_fp(stdout, BIO_NOCLOSE);
-
-  /* ---------------------------------------------------------- *
-   * Create a EC key structure, setting the group type from NID  *
-   * ---------------------------------------------------------- */
-  eccgrp = OBJ_txt2nid(ECCTYPE);
-  myecc = EC_KEY_new_by_curve_name(eccgrp);
-
-  /* -------------------------------------------------------- *
-   * For cert signing, we use  the OPENSSL_EC_NAMED_CURVE flag*
-   * ---------------------------------------------------------*/
-  EC_KEY_set_asn1_flag(myecc, OPENSSL_EC_NAMED_CURVE);
-
-  /* ---------------------------------------------------------- *
    * Read private key from file                                 *
    * ---------------------------------------------------------- */
   FILE *fkey;
-  fkey = fopen("PrivateKey.pem", "rb");
+  fkey = fopen(keyFileName, "rb");
   PEM_read_PrivateKey(fkey, &pkey, NULL, NULL);
-  //PEM_read_bio_PrivateKey(inbio, &pkey, NULL, NULL);
   fclose(fkey);
 
-  //printf("My message: %s\n", pkey);
 
-  //char * privatekey = NULL;
-  //long mylength;
-  //if (fkey)
-  //{
-  //fseek (fkey, 0, SEEK_END);
-  //mylength = ftell (fkey);
-  //fseek (fkey, 0, SEEK_SET);
-  //privatekey = malloc( sizeof (unsigned char) * (mylength));
-  //if (privatekey)
-  //{
-  //fread (privatekey, 1, mylength, fkey);
-  //PEM_read_PrivateKey(fkey, &pkey, NULL, NULL);
-  //printf("Length: %ld\n", mylength);
-  //}
-  //
-  //fclose (fkey);
-  //}
-
-
-  /* -------------------------------------------------------- *
-   * Now we show how to extract EC-specifics from the key     *
-   * ---------------------------------------------------------*/
-  myecc = EVP_PKEY_get1_EC_KEY(pkey);
-  const EC_GROUP *ecgrp = EC_KEY_get0_group(myecc);
-
-  /* ---------------------------------------------------------- *
-   * Here we print the key length, and extract the curve type.  *
-   * ---------------------------------------------------------- */
-  BIO_printf(inbio, "ECC Key size: %d bit\n", EVP_PKEY_bits(pkey));
-  BIO_printf(inbio, "ECC Key type: %s\n", OBJ_nid2sn(EC_GROUP_get_curve_name(ecgrp)));
-
-  /* Create the Message Digest Context */
-  if(!(mdctx = EVP_MD_CTX_create())) goto err;
 
   /* ---------------------------------------------------------- *
    * This reads the contents of the file to be signed           *
@@ -112,59 +64,57 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
       fclose (f);
     }
 
-  //printf("My message: %s\n", msg);
+  printf("File to sign: %s\n", msg);
   /* ---------------------------------------------------------- *
    * If there are no errors, this signs the contents of the file*
    * This will return a digest of the file                      *
    * ---------------------------------------------------------- */
   if (msg)
     {
+
+      /* Create the Message Digest Context */
+      if(!(mdctx = EVP_MD_CTX_create())) printf("There was an error\n");
+      
       /* Initialise the DigestSign operation - SHA-256 has been selected as the message digest function in this example */
-      if (1 != EVP_DigestSignInit(mdctx, NULL, EVP_sha256(), NULL, pkey)) goto err; // Vérifier si ça hashe bien
-      // Vérifier que l'output soit bien cohérent avec la fonction de hachahge
-      // n octets -> 256/8 -> 32
-      // 2 parties à ECDSA
-      // 32 octets x 2 -> r = 32 ; s = 32
-      // clé privé  32 octets
-      // clé publique 32 x 2 octets
-      // montrer que tout est bien cohérent en termes d'algos
-
+      if (1 != EVP_DigestSignInit(mdctx, NULL, EVP_sha256(), NULL, pkey)) printf("There was an error\n"); 
+      
       /* Call update with the message */
-      if(1 != EVP_DigestSignUpdate(mdctx, msg, strlen(msg))) goto err;
-
+      if(1 != EVP_DigestSignUpdate(mdctx, msg, strlen(msg))) printf("There was an error\n");
+      
       /* Finalise the DigestSign operation */
       /* First call EVP_DigestSignFinal with a NULL sig parameter to obtain the length of the */
       /* signature. Length is returned in slen */
-
-      if(1 != EVP_DigestSignFinal(mdctx, NULL, &slen)) goto err;
-
+      
+      if(1 != EVP_DigestSignFinal(mdctx, NULL, &slen)) printf("There was an error\n");
+      
       /* Allocate memory for the signature based on size in slen */
-      if(!(sig = OPENSSL_malloc(sizeof(unsigned char) * (slen)))) goto err;
-
+      if(!(sig = OPENSSL_malloc(sizeof(unsigned char) * (slen)))) printf("There was an error\n");
+      
       /* Obtain the signature */
-      if(1 != EVP_DigestSignFinal(mdctx, sig, &slen)) goto err;
+      if(1 != EVP_DigestSignFinal(mdctx, sig, &slen)) printf("There was an error\n");
       
       printf("%s\n",sig);
       
-//       /* Success */
-//      if(!PEM_write_bio_PrivateKey(inbio, pkey, NULL, NULL, 0, 0, NULL))
-//      BIO_printf( inbio , "Error writing private key data in PEM format" );
-//      ret = 1;
-
-    err:
-      if(ret != 1)
-      {
-        /* Do some error handling */
-      }
+      
+      FILE * fout = fopen (signature_output, "wb");
+      fwrite(sig,1,slen,fout);
+      fclose(fout);
+      
+      printf("%s\n",sig);
+      
     } 
-
+  
   /* ---------------------------------------------------------- *
    * Free up all structures                                     *
    * ---------------------------------------------------------- */
-  if(*sig && !ret) OPENSSL_free(sig);
+  if(*sig) OPENSSL_free(sig);
   if(mdctx) EVP_MD_CTX_destroy(mdctx);
   EVP_PKEY_free(pkey);
   EC_KEY_free(myecc);
-  BIO_free_all(inbio);
+  mxFree(keyFileName);
+  mxFree(filename);
 
+  plhs[0] = mxCreateString(signature_output);
+  
 }
+
