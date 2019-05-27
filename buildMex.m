b2f1f68fc7eb295ae12cfea6ff6bdedfbae2a320
AngeloCa <angelo.canesso@orange.fr>
2019-05-27 15:51:19 +0200

    mac_build

--- buildMex.m
+++ buildMex.m
@@ -29,7 +29,7 @@ function buildMex(enblDbg)
         options = [' C:\openssl\lib\libeay32.lib -IC:\openssl\include ' options];
     elseif(ismac())
         fprintf(1,'Building Mac...\n');
-        options = [options ' -I/usr/local/ssl/include -lssl -lcrypto '];
+        options = [options ' -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include -lssl -lcrypto '];
     elseif(isunix())
         fprintf(1,'Building UNIX...\n');
         options = [options ' -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include -lssl -lcrypto '];
