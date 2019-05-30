40f8b5f1e9baca0aaf49acd79d6b750ea1d8d9a5
leandro <leandrones>
2019-05-30 15:38:11 +0200

    update generate files eith seq loop

--- files/generate_files.sh
+++ files/generate_files.sh
@@ -4,7 +4,7 @@
 # The i-th file is named output_i.dat and has a size of i MB
 # Execute this from inside the files directory
 
-for i in  {1..100..1}
+for i in `seq 1 100`
 do
-    dd if=/dev/urandom of="${i}.txt"  bs=1c  count="${i}"
+    dd if=/dev/urandom of="$i.txt"  bs=1c  count="$i"
 done
